31868aabc3c6931f266601c0e5681b8edc4266a1
Andrii Babushko <andriibabushko@gmail.com>
2022-09-11 11:01:39 +0300

    Complete task 1

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,7 +1,16 @@
-
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+""" Lab 1. Python. Andrii Babushko. Repository: https://github.com/AndriiBabushko/Python """
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# task 1
+def task_1(some_int_list):
+    some_result_list = []
+    for i in range(len(some_int_list)):
+        if 1 <= some_int_list[i] <= 3:
+            some_result_list.append(some_int_list[i])
+
+    return some_result_list
+
+
+task_1_list = [1, 2, 5, 4, 10, 15, 3]
+print(f'Task 1 list: {task_1_list}')
+print(f'List of elems that in interval [1; 3]: {task_1(task_1_list)}')
