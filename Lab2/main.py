dc1f8dac4c610c12be6e194984a62ba1a3105c4a
Andrii Babushko <andriibabushko@gmail.com>
2022-09-11 11:07:19 +0300

    Complete task 2

--- Lab2/main.py
+++ Lab2/main.py
@@ -11,6 +11,28 @@ def task_1(some_int_list):
     return some_result_list
 
 
+# task 2
+def task_2(year):
+    if is_leap_year(year):
+        print(f'This year({year}) has 366 days.')
+    else:
+        print(f'This year({year}) has 365 days.')
+
+
+def is_leap_year(year):
+    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+        return True
+    return False
+
+
+# task 1
+print('\nTASK 1!!!')
 task_1_list = [1, 2, 5, 4, 10, 15, 3]
 print(f'Task 1 list: {task_1_list}')
 print(f'List of elems that in interval [1; 3]: {task_1(task_1_list)}')
+
+# task 2
+print('\nTASK 2!!!')
+task_2_year = [2000, 2001, 1932, 1841, 2004, 2014]
+for i in range(len(task_2_year)):
+    task_2(task_2_year[i])
