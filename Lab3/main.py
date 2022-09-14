858a8b1985fe09b524303195c0409f7d06c4d8a4
Andrii Babushko <andriibabushko@gmail.com>
2022-09-14 18:56:28 +0300

    Complete task 3 in Lab 3

--- Lab3/main.py
+++ Lab3/main.py
@@ -24,13 +24,25 @@ print(f'Count of searching letter({task_1_some_searching_letter}) = '
 
 # task 2
 def task_2_change_text(text):
-    count_changes = text.count(':')
     new_text = text.replace(':', '%')
     print(f'Text after changes: {new_text}')
-    return count_changes
+    return text.count(':')
 
 
-print('\nTASK 1!!!')
+print('\nTASK 2!!!')
 task_2_some_text = ': % % :: %% dfd 1212dffd12121 % :'
 print(f'Task 2 text: "{task_2_some_text}"')
 print(f'Count changes from : to %: {task_2_change_text(task_2_some_text)}')
+
+
+# task 3
+def task_3_delete_dots(text):
+    new_text = text.replace('.', '')
+    print(f'Text after changes: {new_text}')
+    return text.count('.')
+
+
+print('\nTASK 3!!!')
+task_3_some_text = '. ; : ^% $ # @#$ *& )* .. ... . dfsd 123'
+print(f'Task 3 text: "{task_3_some_text}"')
+print(f'Count changes from : to %: {task_3_delete_dots(task_3_some_text)}')
