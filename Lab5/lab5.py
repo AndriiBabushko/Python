8baf8414487b726bb7719989e40ac1fc10d958f7
Andrii Babushko <andriibabushko@gmail.com>
2022-10-01 19:00:40 +0300

    Complete task 3 in Lab 5

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -14,19 +14,17 @@ def task_1_get_rectangles_areas(rectangles_sides):
 def enter_rectangles_sides(counter):
     rectangles_sides = []
 
-    while True:
+    while counter != 0:
         try:
             side_a = float(input(f'Enter length of side a: '))
             side_b = float(input(f'Enter length of side b: '))
             rectangles_sides.append([side_a, side_b])
             if side_a < 0 or side_b < 0:
-                raise ValueError(f'Сторони менше 0!')
+                raise ValueError(f'Sides менше 0!')
             else:
                 pass
-                print('Сторони введено правильно!')
+                print('Sides were entered correctly!')
                 counter -= 1
-            if counter == 0:
-                break
         except ValueError as value_error:
             rectangles_sides.pop()
             print('ERROR:', value_error)
@@ -57,19 +55,17 @@ def task_2_get_right_triangles_hypotenuses(right_triangles_legs):
 def enter_right_triangles_legs(counter):
     right_triangles_legs = []
 
-    while True:
+    while counter != 0:
         try:
             leg_a = float(input(f'Enter leg a: '))
             leg_b = float(input(f'Enter leg b: '))
             right_triangles_legs.append([leg_a, leg_b])
             if leg_a < 0 or leg_b < 0:
-                raise ValueError(f'Сторони менше 0!')
+                raise ValueError(f'Legs are less than 0!')
             else:
                 pass
-                print('Сторони введено правильно!')
+                print('Legs were entered correctly!')
                 counter -= 1
-            if counter == 0:
-                break
         except ValueError as value_error:
             right_triangles_legs.pop()
             print('ERROR:', value_error)
@@ -86,14 +82,87 @@ def compare_hypotenuses(hypotenuses):
 
 
 print('\nTASK 2!!!')
-task_2_right_triangles_legs = enter_right_triangles_legs(2)
-print(f'List of right triangle legs: {task_2_right_triangles_legs}')
-task_2_hypotenuses = task_2_get_right_triangles_hypotenuses(task_2_right_triangles_legs)
-compare_hypotenuses(task_2_hypotenuses)
+
+
+# task_2_right_triangles_legs = enter_right_triangles_legs(2)
+# print(f'List of right triangle legs: {task_2_right_triangles_legs}')
+# task_2_hypotenuses = task_2_get_right_triangles_hypotenuses(task_2_right_triangles_legs)
+# compare_hypotenuses(task_2_hypotenuses)
+
 
 # task 3
+def check_if_point_is_in_circle(checked_point, circle_points_and_radius):
+    equation = (checked_point[0] - circle_points_and_radius[0]) ** 2 + (
+            checked_point[1] - circle_points_and_radius[1]) ** 2
+
+    # print(f'Radius ^2: {circle_points_and_radius[2] ** 2}')
+    # print(f'Equation: {equation}')
+    if (circle_points_and_radius[2] ** 2) == equation:
+        return True
+    return False
+
+
+def enter_circle_center_points_and_radius():
+    point_a = 0
+    point_b = 0
+    circle_radius = 0
+
+    while True:
+        try:
+            point_a = float(input(f'Enter circle center point a: '))
+            point_b = float(input(f'Enter circle center point b: '))
+            circle_radius = float(input(f'Enter radius: '))
+            if circle_radius < 0:
+                raise ValueError(f'Radius is less than 0!')
+            else:
+                pass
+                print('Center circle points and radius were entered correctly!')
+                break
+        except ValueError as value_error:
+            print('ERROR:', value_error)
+
+    return [point_a, point_b, circle_radius]
+
+
+def enter_some_point(point):
+    point_a = 0
+    point_b = 0
+
+    while True:
+        try:
+            if point == 0:
+                point_a = float(input(f'Enter point p1 of P: '))
+                point_b = float(input(f'Enter point p2 of P: '))
+            if point == 1:
+                point_a = float(input(f'Enter point f1 of F: '))
+                point_b = float(input(f'Enter point f2 of F: '))
+            if point == 2:
+                point_a = float(input(f'Enter point l1 of L: '))
+                point_b = float(input(f'Enter point l2 of L: '))
+            break
+        except ValueError as value_error:
+            print('ERROR:', value_error)
+
+    return [point_a, point_b]
+
 
 print('\nTASK 3!!!')
+counter_point_in_circle = 0
+counter_point_out_of_circle = 0
+task_3_circle_center_and_radius = enter_circle_center_points_and_radius()
+print(f'Center point O({task_3_circle_center_and_radius[0]}, {task_3_circle_center_and_radius[1]}).'
+      f' Radius: {task_3_circle_center_and_radius[2]}')
+
+for i in range(0, 3):
+    task_3_some_point = enter_some_point(i)
+    if check_if_point_is_in_circle(task_3_some_point, task_3_circle_center_and_radius):
+        counter_point_in_circle += 1
+    else:
+        counter_point_out_of_circle += 1
+
+print(f'Count of points which are in circle: {counter_point_in_circle}')
+print(f'Count of points which are out of circle: {counter_point_out_of_circle}')
+
 
 # task 4
 
