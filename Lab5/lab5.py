48fdb50b3e88d62c1b73b3cc73cf482f9a13f393
Andrii Babushko <andriibabushko@gmail.com>
2022-10-01 15:50:38 +0300

    Complete task 2 in Lab 5

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -35,15 +35,61 @@ def enter_rectangles_sides(counter):
 
 
 print('\nTASK 1!!!')
-task_1_rectangles_sides = enter_rectangles_sides(2)
-print(f'List of rectangles sides: {task_1_rectangles_sides}')
-task_1_rectangles_areas = task_1_get_rectangles_areas(task_1_rectangles_sides)
-for rectangle in range(0, len(task_1_rectangles_areas)):
-    print(f'{rectangle + 1} rectangle\'s area: {task_1_rectangles_areas[rectangle]}')
+
+
+# task_1_rectangles_sides = enter_rectangles_sides(3)
+# print(f'List of rectangles sides: {task_1_rectangles_sides}')
+# task_1_rectangles_areas = task_1_get_rectangles_areas(task_1_rectangles_sides)
+# for rectangle in range(0, len(task_1_rectangles_areas)):
+#     print(f'{rectangle + 1} rectangle\'s area: {task_1_rectangles_areas[rectangle]}')
+
 
 # task 2
+def task_2_get_right_triangles_hypotenuses(right_triangles_legs):
+    hypotenuses = []
+
+    for i in range(0, len(right_triangles_legs)):
+        hypotenuses.append(round((right_triangles_legs[i][0] ** 2 + right_triangles_legs[i][1] ** 2) ** 0.5, 2))
+
+    return hypotenuses
+
+
+def enter_right_triangles_legs(counter):
+    right_triangles_legs = []
+
+    while True:
+        try:
+            leg_a = float(input(f'Enter leg a: '))
+            leg_b = float(input(f'Enter leg b: '))
+            right_triangles_legs.append([leg_a, leg_b])
+            if leg_a < 0 or leg_b < 0:
+                raise ValueError(f'Сторони менше 0!')
+            else:
+                pass
+                print('Сторони введено правильно!')
+                counter -= 1
+            if counter == 0:
+                break
+        except ValueError as value_error:
+            right_triangles_legs.pop()
+            print('ERROR:', value_error)
+
+    return right_triangles_legs
+
+
+def compare_hypotenuses(hypotenuses):
+    for i in range(0, len(hypotenuses) - 1):
+        if hypotenuses[i] > hypotenuses[i + 1]:
+            print(f'Hypotenuse №{i + 1}({hypotenuses[i]}) > Hypotenuse №{i + 2}({hypotenuses[i + 1]})')
+        else:
+            print(f'Hypotenuse №{i + 1}({hypotenuses[i]}) < Hypotenuse №{i + 2}({hypotenuses[i + 1]})')
+
 
 print('\nTASK 2!!!')
+task_2_right_triangles_legs = enter_right_triangles_legs(2)
+print(f'List of right triangle legs: {task_2_right_triangles_legs}')
+task_2_hypotenuses = task_2_get_right_triangles_hypotenuses(task_2_right_triangles_legs)
+compare_hypotenuses(task_2_hypotenuses)
 
 # task 3
 
